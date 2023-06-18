3859ee4156971ab12431577eb0bb715f75e01efd
arutaka1220 <97819325+arutaka1220@users.noreply.github.com>
2023-06-18 19:57:43 +0900

    add Block/Item Component Interface

---
+++ src/wrapper/Types/BlockComponents.ts
@@ -0,0 +1,10 @@
+import * as Minecraft from "@minecraft/server";
+
+export interface BlockComponents {
+    "minecraft:piston": Minecraft.BlockPistonComponent,
+    "piston": Minecraft.BlockPistonComponent,
+    "minecraft:recordPlayer": Minecraft.BlockRecordPlayerComponent,
+    "recordPlayer": Minecraft.BlockRecordPlayerComponent,
+    "minecraft:sign": Minecraft.BlockSignComponent,
+    "sign": Minecraft.BlockSignComponent,
+}
---
+++ src/wrapper/Types/ItemComponents.ts
@@ -0,0 +1,12 @@
+import * as Minecraft from "@minecraft/server"
+
+export interface ItemComponents {
+    "minecraft:cooldown": Minecraft.ItemCooldownComponent,
+    "cooldown": Minecraft.ItemCooldownComponent,
+    "minecraft:durability": Minecraft.ItemDurabilityComponent,
+    "durability": Minecraft.ItemDurabilityComponent,
+    "minecraft:enchantments": Minecraft.ItemEnchantsComponent,
+    "enchantments": Minecraft.ItemEnchantsComponent,
+    "minecraft:food": Minecraft.ItemFoodComponent,
+    "food": Minecraft.ItemFoodComponent,
+}
